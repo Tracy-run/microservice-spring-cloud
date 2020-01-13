f8b9c3013c203890adbf0aadbdccc44db8821e76
Tracy--RUN <2463136713@qq.com>
2020-01-13 18:25:14 +0800

    zuul  + ribbon

---
+++ microservice-spring-ribbon/src/main/java/com/bus/ribbon/microservicespringribbon/MicroserviceSpringRibbonApplication.java
@@ -0,0 +1,13 @@
+package com.bus.ribbon.microservicespringribbon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceSpringRibbonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroserviceSpringRibbonApplication.class, args);
+    }
+
+}
---
+++ microservice-spring-ribbon/src/test/java/com/bus/ribbon/microservicespringribbon/MicroserviceSpringRibbonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bus.ribbon.microservicespringribbon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceSpringRibbonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
